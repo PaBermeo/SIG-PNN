7a988b5738f7ef49eab5d380d1b565d7b985bbd2
PAULA ANDREA BERMEO FÚQUENE <pbermeo@agrosavia.co>
2026-03-10 04:59:44 -0500

    Percetage loss areas calculated

--- Scripts/1_Use_land_temporal_change.R
+++ Scripts/1_Use_land_temporal_change.R
@@ -27,6 +27,8 @@ Fincas1 <- project(Fincas1, crs(LU_86))
 F1 <- Fincas1[Fincas1$DIRECCION == "LA FLORESTA", ] 
 F1_sf <- sf::st_as_sf(F1)
 
+
+
 #Mask variables to the ca (Calibration Area)
 F1_86 <- mask(crop(LU_86, F1), F1)
 F1_24 <- mask(crop(LU_24, F1), F1)
@@ -102,28 +104,132 @@ bbox_exp <- expand_bbox(F1_86, frac = 0.3)
  L <- st_as_sf(Loss1, merge = T)
  LossF <- L[L$classification_1986 == 1, ]
  
- st_write(LossF, "Vectorial/Gananc_perdd_finca/PerdidaBosq_Fincaname.shp",  overwrite = TRUE)
+ st_write(LossF, "Vectorial/Ganancia_bosque/LA FLORESTA.shp",  overwrite = TRUE)
 
-#Gnancia entre el año   
+#Ganancia entre 1985 a 2024   
  Gain1 <- st_as_stars(Gain, proxy=T) 
  G <- st_as_sf(Gain1, merge = T)
  GainF <- G[G$classification_1986 == 1, ]
  
- st_write(GainF, "Vectorial/Gananc_perdd_finca/GanaBosq_Fincaname.shp",  overwrite = TRUE)
+ st_write(GainF, "Vectorial/Perdida_bosque/LA FLORESTA.shp",  overwrite = TRUE)
  
 #Cálculo de áreas de pérdida y ganancia por finca
  
 #Recortar loss/gain de acuerdo al shapefile de cada finca
 
- Cambio_m <- project(Loss, "EPSG:3116")
+ Cambio_m <- project(Loss, "EPSG:3116", method="near") #sí el método es bilinear, genera un promedio o interpolación
  F1_m <- project(F1, "EPSG:3116")
+
+ F1_m_sf <- sf::st_as_sf(F1_m)
+ 
+ area_finca <- sf::st_area(F1_m_sf) / 10000
+ area_finca
  
 # Aumentar resolución
-Cambio_fino <- disagg(Cambio_m, fact=40)
+Cambio_fino <- disagg(Cambio_m, fact= 40)
  
 # Ahora aplicar mask
 Cambio_rec <- mask(crop(Cambio_fino, F1_m), F1_m)
-plot(Cambio_rec)
+#plot(Cambio_rec)
+
+LossFin <- st_as_stars(Cambio_rec, proxy=T) 
+L <- st_as_sf(LossFin, merge = T)
+LossFino <- L[L$classification_1986 == 1, ]
+
+st_write(LossFino, "Vectorial/Perdida_bosque/LA FLORESTA_fino.shp",  overwrite = TRUE)
+
+ #cálculo de areas de acuerdo con script PMP ENM 
+
+area_r <- cellSize(Cambio_rec, unit = "ha")
+
+area_loss <- global(area_r * (Cambio_rec==1), sum, na.rm = TRUE)
+
+area_loss #Área perdida en ha entre el año 2085 a 2024
+
+
+
+
+#Cálculo de porcentaje de área ganada
+Cambio_g <- project(Gain, "EPSG:3116", method="near")
+
+Cam_g <- disagg(Cambio_g, fact= 40)
+
+# Ahora aplicar mask
+Cam_gr <- mask(crop(Cam_g, F1_m), F1_m)
+
+GainFin <- st_as_stars(Cam_gr, proxy=T) 
+G <- st_as_sf(GainFin, merge = T)
+GainFino <- G[G$classification_1986 == 1, ]
+
+st_write(LossFino, "Vectorial/Ganancia_bosque/LA FLORESTA_fino.shp",  overwrite = TRUE)
 
 #cálculo de areas de acuerdo con script PMP ENM 
 
+area_g <- cellSize(Cam_gr, unit = "ha")
+
+area_gain <- global(area_g * (Cam_gr==1), sum, na.rm = TRUE)
+
+area_gain #Área ganada de bosque en ha entre el año 1985 a 2024
+
+
+
+
+# Resultados tabla automatizado -------------------------------------------
+
+# reproyectar fincas a CRS del raster
+Fincas_proj <- project(Fincas1, crs(LU_86))
+
+# tabla de resultados
+resultados <- data.frame(
+  finca = character(),
+  area_finca_ha = numeric(),
+  area_loss_ha = numeric(),
+  perc_loss = numeric()
+)
+
+for(i in 1:nrow(Fincas_proj)){
+
+  
+  finca_i <- Fincas_proj[i]
+  
+  # nombre de la finca
+  nombre <- finca_i$DIRECCION
+  
+  # área finca
+  finca_m <- project(finca_i, "EPSG:3116")
+  area_finca <- as.numeric(st_area(st_as_sf(finca_m)) / 10000)
+  
+  # recorte raster
+  r86 <- mask(crop(LU_86, finca_i), finca_i)
+  r24 <- mask(crop(LU_24, finca_i), finca_i)
+  
+  # bosque
+  bosq86 <- r86 %in% c(3,13)
+  bosq24 <- r24 %in% c(3,13)
+  
+  # pérdida
+  loss <- bosq86 & !bosq24
+  
+  # reproyección métrica
+  loss_m <- project(loss, "EPSG:3116", method="near")
+  
+  # área por pixel
+  area_pix <- cellSize(loss_m, unit="ha")
+  
+  # área pérdida
+  area_loss <- global(area_pix * (loss_m==1), sum, na.rm=TRUE)[1,1]
+  
+  # porcentaje
+  perc_loss <- (area_loss / area_finca) * 100
+  
+  # guardar resultados
+  resultados <- rbind(resultados, data.frame(
+    finca = nombre,
+    area_finca_ha = area_finca,
+    area_loss_ha = area_loss,
+    perc_loss = perc_loss
+  ))
+  
+}
+
+resultados
